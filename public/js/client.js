84b11a2588d0732c3f381050a0dca93b747aaae5
System <none>
2017-09-20 13:02:27 +0000

    Checkpoint

--- public/js/client.js
+++ public/js/client.js
@@ -136,14 +136,49 @@ var boardButtonCallback = function(t){
     title: 'Popup List Example',
     items: [
       {
-        text: 'Open Overlay',
+        text: 'Open Modal',
         callback: function(t){
-          return t.overlay({
-            url: './overlay.html',
-            args: { rand: (Math.random() * 100).toFixed(0) }
+          return t.modal({
+            // the url to load for the iframe
+            url: './modal.html',
+            // optional arguments to be passed to the iframe as query parameters
+            // access later with t.arg('text')
+            args: { text: 'Hello' },
+            // optional color for header chrome
+            accentColor: '#F2D600',
+            // initial height for iframe
+            height: 500, // not used if fullscreen is true
+            // whether the modal should stretch to take up the whole screen
+            fullscreen: true,
+            // optional function to be called if user closes modal (via `X` or escape, etc)
+            callback: () => console.log('Goodbye.'),
+            // optional title for header chrome
+            title: 'Hello, Modal!',
+            // optional action buttons for header chrome
+            // max 3, up to 1 on right side
+            actions: [{
+              icon: GRAY_ICON,
+              url: 'https://google.com',
+              alt: 'Leftmost',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: (tr) => tr.popup({
+                title: 'Settings',
+                url: 'settings.html',
+                height: 164,
+              }),
+              alt: 'Second from left',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: () => console.log(':tada:'),
+              alt: 'Right side',
+              position: 'right',
+            }],
           })
           .then(function(){
-            return t.closePopup();
+            return t.closeModal();
           });
         }
       },
