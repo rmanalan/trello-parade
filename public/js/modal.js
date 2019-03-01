a33945684af653ad6bfb8685ddcecd303debce31
Glitch (trello-power-up) <none>
2019-03-01 22:49:27 +0000

    🎱🐾 Checkpoint ./public/js/modal.js:9618/317 ./public/modal.html:9618/838

--- public/js/modal.js
+++ public/js/modal.js
@@ -6,6 +6,11 @@ const t = TrelloPowerUp.iframe();
 const text = t.arg('text');
 
 t.render(async function(){
-  const data = await t.getAll();
+  const data = await t.lists('id', 'name');
   console.log(888,data);
+  
+  window.ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
 });
