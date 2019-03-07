f73bcb26b4a4f2c4e6f0b76c4d001a804f130cd1
Glitch (trello-power-up) <none>
2019-03-07 21:33:50 +0000

    🏥🏄 Checkpoint ./src/list-selector.js:9618/44 ./src/app.js:9618/114 ./src/index.css:9618/20

--- src/list-selector.js
+++ src/list-selector.js
@@ -4,7 +4,7 @@ function ListSelector({ lists, onChange }) {
   return (
     <>
       <label htmlFor="selected-list">List to put on parade:</label>
-      <select name="selected-list" onChange={onChange.bind(this.)} >
+      <select name="selected-list" onChange={onChange} >
         <option></option>
         { lists.map(list => <option key={list.id} value={list.id}>{list.name}</option>) }
       </select>
