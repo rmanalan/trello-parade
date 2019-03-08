31a1d7b55fbe785d2886e08d4d987652a3c7aa92
Glitch (trello-power-up) <none>
2019-03-08 01:41:46 +0000

    👍🐆 Checkpoint ./src/card-on-display.js:9618/854

--- src/card-on-display.js
+++ src/card-on-display.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import _ from 'lodash';
 
 function CardOnDisplay({ card }) {
-  const ytid = card.attachments[0].url.split('=')[1];
+  debugger
+  const ytid = _.find(card.attachments, attachment => /youtu/.test(attachment.url));
   return (
-    <iframe 
-      className="vid"
-      src={`https://www.youtube.com/embed/${ytid}?autoplay=1`}
-      frameborder="0" 
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" 
-      allowfullscreen />
+    <>
+      { ytid.length > 0 ?
+        <iframe 
+          className="vid"
+          src={`https://www.youtube.com/embed/${ytid.url.split('=')[1]}?autoplay=1`}
+          frameborder="0" 
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" 
+          allowfullscreen /> :
+         <div>YouTube video not attached</div>
+      }
+    </>
   );
 }
 
