48886e6ece8e19dec7c7d9d30cd884c82dc1fd1c
Glitch (trello-parade) <none>
2019-03-11 16:50:23 +0000

    🌭🎷 Checkpoint ./src/card-on-display.js:9618/814 ./src/app.js:9618/32 ./src/app.css:9618/17

--- src/card-on-display.js
+++ src/card-on-display.js
@@ -9,13 +9,23 @@ function CardOnDisplay({ card }) {
   const ytAttachment = _.find(card.attachments, attachment => re.test(attachment.url));
   
   ytid = ytAttachment ? ytAttachment.url.match(re)[4] : null;
-  ytid = ytid ? ytid.split('=').slice(-1) : null;
+  ytid = ytid ? ytid.split('=').slice(-1)[0] : null;
+  
+  if (ytAttachment) {
+    const params = new URLSearchParams(ytAttachment.url);
+    if (params) {
+      ytid = params.get('v');
+    } else {
+      ytid = ytAttachment ? ytAttachment.url.match(re)[4] : null;
+      ytid = ytid ? ytid.split('=').slice(-1)[0] : null;
+    }
+  }
   
   // Can't find in attachments... look in description
   if (!ytid) {
-    debugger;
-    ytid = card.desc.match(re)[4];
-    ytid = ytid ? ytid.split('=').slice(-1) : null;
+    debugger
+    ytid = re.test(card.desc) ? card.desc.match(re)[4] : null;
+    ytid = ytid ? ytid.split('=').slice(-1)[0] : null;
   }
     
   return (
@@ -27,7 +37,14 @@ function CardOnDisplay({ card }) {
           frameborder="0" 
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" 
           allowfullscreen /> :
-         <div className="presenting-live">No YouTube video attached. Presenting live!</div>
+         <div className="row center-xs middle-xs presenting-live">
+           <div class="col-xs-6">
+             <div class="box">
+               No YouTube video attached. Presenting live!
+             </div>
+           </div>
+         </div>
+         
       }
     </>
   );
